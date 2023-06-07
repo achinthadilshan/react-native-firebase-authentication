c4f54eed26f61cbb5638b57b4e9d0ae04f3a7e0f
ACHii <>
2023-06-07 13:33:01 +0530

    update

--- firebase.js
+++ firebase.js
@@ -1,17 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
+import {
+   FIREBASE_API_KEY,
+   FIREBASE_AUTH_DOMAIN,
+   FIREBASE_PROJECT_ID,
+   FIREBASE_STORAGE_BUCKET,
+   FIREBASE_MESSAGING_SENDER_ID,
+   FIREBASE_APP_ID,
+} from '@env'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-   apiKey: 'AIzaSyD7GHTzZoy8W8KkIH4GPtMhouPkd0XoUBA',
-   authDomain: 'react-native-authenticat-6df03.firebaseapp.com',
-   projectId: 'react-native-authenticat-6df03',
-   storageBucket: 'react-native-authenticat-6df03.appspot.com',
-   messagingSenderId: '154877310081',
-   appId: '1:154877310081:web:e0c09a7c97ee1d533d0980',
+   apiKey: FIREBASE_API_KEY,
+   authDomain: FIREBASE_AUTH_DOMAIN,
+   projectId: FIREBASE_PROJECT_ID,
+   storageBucket: FIREBASE_STORAGE_BUCKET,
+   messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+   appId: FIREBASE_APP_ID,
 }
 
 // Initialize Firebase
